b364b49a1bc911b588c38f6d33f127523f8727bf
prasetyo <prasetyo531@gmail.com>
2025-04-29 12:48:16 +0700

    ini yarn and cypress

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://www.coursera.org/',
+    viewportWidth: 1200,
+    viewportHeight: 800,
+    setupNodeEvents(on, config) {
+      const options = {
+        // Adjust the path to your webpack.config.js
+        webpackOptions: require('../cypress-repo/webpack.config.js'), // Change this line
+        watchOptions: {},
+      };
+      on('file:preprocessor', webpackPreprocessor(options));
+    },
+    specPattern: 'cypress/e2e/scenarios/**/*.{js,ts}', // Adjust the pattern if needed
+    fixturesFolder: 'cypress/e2e/fixtures', // Update the path if you have fixtures
+    defaultCommandTimeout: 10000,
+    chromeWebSecurity: false,
+  },
+});
