3b6ae3c2683192307ae4995df03eb94533e98ec1
prasetyo <prasetyo531@gmail.com>
2025-05-01 09:17:58 +0700

    add command to merge mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -2,22 +2,24 @@ const { defineConfig } = require('cypress');
 const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 
 module.exports = defineConfig({
+  viewportWidth: 1200,
+  viewportHeight: 800,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportPageTitle: "Automated Test Status",
+    reportTitle: "Automated Test Reporting Dashboard",
+    showPassed: "true",
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true
+},
   e2e: {
     baseUrl: 'https://www.coursera.org/',
-    viewportWidth: 1200,
-    viewportHeight: 800,
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportPageTitle: "Automated Test Status",
-      reportTitle: "Automated Test Reporting Dashboard",
-      showPassed: "true",
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: true,
-      json: true,
-      charts: true,
-  },
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       const options = {
         // Adjust the path to your webpack.config.js
         webpackOptions: require('../cypress-repo/webpack.config.js'), // Change this line
@@ -26,8 +28,6 @@ module.exports = defineConfig({
       on('file:preprocessor', webpackPreprocessor(options));
     },
     specPattern: 'cypress/e2e/scenarios/**/*.{js,ts}', // Adjust the pattern if needed
-    fixturesFolder: 'cypress/e2e/fixtures', // Update the path if you have fixtures
-    defaultCommandTimeout: 10000,
-    chromeWebSecurity: false,
+    fixturesFolder: 'cypress/e2e/fixtures' // Update the path if you have fixtures
   },
 });
